52427e707d128cbe281d5732726c4386bc734838
eugeneath <eugeneath@yandex.ru>
2017-10-16 21:13:23 +0700

    replace footer from intro module into individual module

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 import renderBlock from './render-block';
 import moduleIntro from './module_intro';
+import getElementFromTemplate from './get-element';
+import footerTemplate from './footer';
 
-const ready = () => renderBlock(moduleIntro);
+const main = document.querySelector(`main`);
+const ready = () => {
+  renderBlock(moduleIntro);
+  main.appendChild(getElementFromTemplate(footerTemplate));
+};
 
 document.addEventListener(`DOMContentLoaded`, ready);

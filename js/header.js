6a3ab0b96ffa0ca1ac1d1f03e97381803f1ba17c
eugeneath <eugeneath@yandex.ru>
2017-10-18 19:27:26 +0700

    start task 2

--- js/header.js
+++ js/header.js
@@ -1,3 +1,8 @@
+const initialState = {
+  lives: 3,
+  time: 0
+};
+
 const headerTemplate = `<header class="header">
     <div class="header__back">
       <button class="back">
@@ -5,11 +10,14 @@ const headerTemplate = `<header class="header">
         <img src="img/logo_small.svg" width="101" height="44">
       </button>
     </div>
-    <h1 class="game__timer">NN</h1>
+    <h1 class="game__timer">${initialState.time}</h1>
     <div class="game__lives">
-      <img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
-      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
-      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+    ${new Array(3 - initialState.lives)
+      .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
+      .join(``)}
+    ${new Array(initialState.lives)
+      .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
+      .join(``)}
     </div>
   </header>`;
 

b282ff99ceae5312735d8918720c02da55e66cea
eugeneath <eugeneath@yandex.ru>
2017-10-22 21:06:01 +0700

    avoid duplicating images in game3; avoid situation, when no paints in a row

--- js/module_game-3.js
+++ js/module_game-3.js
@@ -29,6 +29,8 @@ const moduleGame3 = () => getElementFromTemplate(`${headerTemplate(currentState)
   </div>
 ${footerTemplate}`);
 
+const getRandomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
 const getGame3 = () => {
   renderBlock(moduleGame3());
 
@@ -51,12 +53,30 @@ const getGame3 = () => {
   });
   document.querySelector(`.back`).addEventListener(`click`, () => renderBlock(moduleIntro));
 
-  options.forEach((t) => {
-    const imgData = getRandomImage();
-    const img = t.querySelector(`img`);
-    img.src = imgData.imageUrl;
-    img.dataset.type = imgData.type;
-  });
+  const img1 = options[0].querySelector(`img`);
+  const img2 = options[1].querySelector(`img`);
+  const img3 = options[2].querySelector(`img`);
+  const imgData1 = getRandomImage();
+  let imgData2 = getRandomImage();
+  let imgData3 = getRandomImage();
+  img1.src = imgData1.imageUrl;
+  img1.dataset.type = imgData1.type;
+  while (imgData1.imageUrl === imgData2.imageUrl) {
+    imgData2 = getRandomImage();
+  }
+  img2.src = imgData2.imageUrl;
+  img2.dataset.type = imgData2.type;
+  while (imgData1.imageUrl === imgData3.imageUrl || imgData2.imageUrl === imgData3.imageUrl) {
+    imgData3 = getRandomImage();
+  }
+  img3.src = imgData3.imageUrl;
+  img3.dataset.type = imgData3.type;
+  while (imgData1.type === `photo` && imgData2.imageUrl === `photo` && imgData3.imageUrl === `photo`) {
+    const imgDataX = getRandomImage();
+    const imgX = options[getRandomNumber(0, 2)].querySelector(`img`);
+    imgX.src = imgDataX.imageUrl;
+    imgX.dataset.type = imgDataX.type;
+  }
 };
 
 export default getGame3;

90058af8d5add1e15917de51f2d3b4f6952ef77e
eugeneath <eugeneath@yandex.ru>
2017-10-31 14:01:55 +0700

    hey, MVP, catch a small plantain

--- js/games/game.js
+++ js/games/game.js
@@ -8,31 +8,33 @@ class GameScreen {
     this.view = gameBlock;
     changeView(this.view);
 
-    const timer = setInterval(() => {
+    this.timer = setInterval(() => {
       this.tick();
     }, 1000);
 
-    this.view.onAnswer = (answers) => {
-      clearInterval(timer);
-      let answer = Answer.CORRECT;
-      const isWrongAnswer = () => question.data.some((t, i) => t.type !== answers[i]);
-      if (!answers || isWrongAnswer()) {
-        answer = Answer.WRONG;
-      } else {
-        if (AnswerTime.MAX - gameBlock.state.time < AnswerTime.FAST) {
-          answer = Answer.FAST;
-        } else if (AnswerTime.MAX - gameBlock.state.time > AnswerTime.SLOW) {
-          answer = Answer.SLOW;
-        }
-      }
-      const nextState = getNextState(state, answer);
-      getNextScreen(nextState);
-    };
+    this.view.onAnswer = (answers) => this.onAnswer(state, question, answers);
 
     this.view.onBackBtnClick = () => {
-      clearInterval(timer);
+      clearInterval(this.timer);
       repeatGame();
     };
+  }
+
+  onAnswer(state, question, answers) {
+    clearInterval(this.timer);
+    let answer = Answer.CORRECT;
+    const isWrongAnswer = () => question.data.some((t, i) => t.type !== answers[i]);
+    if (!answers || isWrongAnswer()) {
+      answer = Answer.WRONG;
+    } else {
+      if (AnswerTime.MAX - this.view.state.time < AnswerTime.FAST) {
+        answer = Answer.FAST;
+      } else if (AnswerTime.MAX - this.view.state.time > AnswerTime.SLOW) {
+        answer = Answer.SLOW;
+      }
+    }
+    const nextState = getNextState(state, answer);
+    getNextScreen(nextState);
   }
 
   tick() {

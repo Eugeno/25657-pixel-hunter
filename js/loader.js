205cf83a8b0887ae5f5a6230a64e90b32f2a8e67
eugeneath <eugeneath@yandex.ru>
2017-11-01 14:54:18 +0700

    made preloader work correctly

--- js/loader.js
+++ js/loader.js
@@ -1,23 +1,24 @@
 const URL = `https://es.dump.academy/pixel-hunter/questions`;
 
 const preloadImages = (sources) => {
-  let counter = 0;
-  let total = 0;
-  const onLoad = () => {
-    counter++;
-    if (counter === total) {
-      console.log(`Я всё!`);
-    }
-  };
-  sources.forEach((question) => {
-    total += question.data.length;
-    question.data.forEach((image) => {
-      const img = document.createElement(`img`);
-      img.onload = img.onerror = onLoad;
-      img.src = image.src;
+  return new Promise((resolve) => {
+    let counter = 0;
+    let total = 0;
+    const onLoad = () => {
+      counter++;
+      if (counter === total) {
+        resolve(sources);
+      }
+    };
+    sources.forEach((question) => {
+      total += question.data.length;
+      question.data.forEach((image) => {
+        const img = document.createElement(`img`);
+        img.onload = img.onerror = onLoad;
+        img.src = image.src;
+      });
     });
   });
-  return sources;
 };
 
 class Loader {
